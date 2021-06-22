60bbe08632fb6d788acc3f8897c7ca6d52d2b15f
Wills Liou <willsliou@protonmail.com>
2021-06-22 13:05:25 -0700

    fixed paranthesis around x_append. begin epoch training

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ data = web.DataReader(company, 'yahoo', start, end)
 
 # Prepare data 
 # Fit data into 0 to 1 using sk.learnpreprocessor module
-scaler = MinMaxScaler(feature_range(0, 1))
+scaler = MinMaxScaler(feature_range=(0, 1))
 # Predict closing price
 scaled_data = scaler.fit_transform(data['Close'].values.reshape(-1, 1))
 
@@ -39,10 +39,11 @@ y_train = []
 
 for x in range(prediction_days, len(scaled_data)):
     x_train.append(scaled_data[x-prediction_days:x, 0])
-    x_train.append(scaled_data[x, 0])
+    y_train.append(scaled_data[x, 0])
 
-x_train, y_train = np.array(x_train, np.array(y_train))
-x_train = np.reshape(x_train, x_train.shape[0], x_train.shape[1])
+x_train = np.array(x_train)
+y_train = np.array(y_train)
+x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1) )
 
 #Building model
 model = Sequential()
