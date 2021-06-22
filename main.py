194fc7edf5b00d148745f9070e949a6df324e554
Wills Liou <willsliou@protonmail.com>
2021-06-22 12:34:49 -0700

    model

--- main.py
+++ main.py
@@ -29,4 +29,36 @@ data = web.DataReader(company, 'yahoo', start, end)
 # Fit data into 0 to 1 using sk.learnpreprocessor module
 scaler = MinMaxScaler(feature_range(0, 1))
 # Predict closing price
-scalred_data = scaler.fit_transform(data['Close'].values)
+scaled_data = scaler.fit_transform(data['Close'].values.reshape(-1, 1))
+
+prediction_days = 60
+
+# Prepare training data
+x_train = []
+y_train = []
+
+
+for x  in range(prediction_days, len(scaled_data)):
+    x_train.append(scaled_data[x-prediction_days:x, 0])
+    x_train.append(scaled_data[x, 0])
+
+x_train, y_train = np.array(x_train, np.array(y_train))
+x_train = np.reshape(x_train, x_train.shape[0], x_train.shape[1])
+
+#Building model
+model = Sequential()
+
+
+model.add(LSTM(units = 50, return_sequences = True, input_shape=(x_train.shape[1], 1) ) )
+
+
+
+
+
+
+
+
+
+
+
+
